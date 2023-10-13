dd113a7ceab7b73e62184ece192c0de90a8a85d4
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2023-10-13 00:38:53 -0400

    Ajout ZoomCustom

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -16,23 +16,51 @@
 				maxZoom: 19,
 				minZoom: -10,
 				zoom: 1,
-				zoomControl: false
+				zoomControl: false,
+				attributionControl: false
 			})
 			L.imageOverlay(appContext.archive, bounds).addTo(map)
 			map.fitBounds(bounds)
-			map.addControl(L.control.zoom({ position: 'topright' }))
+			//map.addControl(L.control.zoom({ position: 'topright' }))
 			let zones = new L.FeatureGroup()
 			map.addLayer(zones)
 
+			L.Control.CustomZoom = L.Control.extend({
+				onAdd: function(map) {
+					let wrap = L.DomUtil.create('div')
+					wrap.classList.add('zoomWrap')
+					let btn1 = L.DomUtil.create('button')
+					btn1.innerText = "+"
+					btn1.classList.add('btnZoomIn')
+				 	wrap.appendChild(btn1)
+					btn1.addEventListener('click', () => map.zoomIn(0.5))
 
+					let btn2 = L.DomUtil.create('button')
+					btn2.innerText = "-"
+					btn2.classList.add('btnZoomOut')
+					wrap.appendChild(btn2)
+					btn2.addEventListener('click', () => map.zoomOut())
+
+					return wrap;
+				},
+
+				onRemove: function(map) {
+					// Nothing to do here
+				}
+			})
+
+			L.control.customZoom = function(opts) {
+				return new L.Control.CustomZoom(opts);
+			}
+			L.control.customZoom({ position: 'topright' }).addTo(map);
 			const options = {
 				position: 'bottomright',
 				draw: {
 					polygon: {
 						allowIntersection: true,
 						drawError: {
-							color: '#e1e100', // Color the shape will turn when intersects
-							message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+							color: '#e1e100',
+							message: 'Erreur! Modifiez la forme de cette zone!'
 						},
 						shapeOptions: {
 							color: '#bada55'
@@ -54,7 +82,8 @@
 
 			map.on(L.Draw.Event.CREATED, function (e) {
 				zones.addLayer(e.layer)
-				console.log(e.layer)
+				console.log(e.layer._latlngs[0])
+				console.log(zones._layers)
 			});
 		}
 		img.src = appContext.archive
